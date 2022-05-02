8032455c5a62bd49cf734c545fff41b73db90b62
ariqbasyar <ariqbasyar2@gmail.com>
2022-05-02 22:49:09 +0700

    added kafka docker using bitnami

--- kafka/start-broker.sh
+++
@@ -1,3 +0,0 @@
-ROOT_PROJECT=/home/ariqbasyar/kafka_2.13-3.1.0
-
-$ROOT_PROJECT/bin/kafka-server-start.sh $ROOT_PROJECT/config/server.properties
--- kafka/start-zookeeper.sh
+++
@@ -1,3 +0,0 @@
-ROOT_PROJECT=/home/ariqbasyar/kafka_2.13-3.1.0
-
-$ROOT_PROJECT/bin/zookeeper-server-start.sh $ROOT_PROJECT/config/zookeeper.properties
