b642f7933f78704b0610ea8acd28ab02fb416be9
ariqbasyar <ariqbasyar2@gmail.com>
2022-06-20 20:29:52 +0700

    input for scenario 2,3, and 4 is ready

--- input/producer.py
+++ input/producer.py
@@ -1,39 +1,26 @@
 import asyncio
 import os
-import uuid
 import cv2
 
-from datetime import datetime
 import numpy as np
 
 from fogverse import ConsumerStorage, Producer, OpenCVConsumer
 from fogverse.logging import CsvLogging
+from fogverse.util import get_cam_id, get_timestamp_str
 
-rstate = np.random.RandomState(42)
-# SIZE = (10,10,3)
-# SIZE = (100,100,3)
-SIZE = (480,640,3)
-# SIZE = (1280,720,3)
-# SIZE = (1080,1920,3)
+SIZE = (640,480,3)
 
-class MyConsumer(OpenCVConsumer, ConsumerStorage):
+class MyStorage(OpenCVConsumer, ConsumerStorage):
     def __init__(self):
         OpenCVConsumer.__init__(self)
         ConsumerStorage.__init__(self)
-        self.consumer.set(cv2.CAP_PROP_FRAME_WIDTH, SIZE[1])
-        self.consumer.set(cv2.CAP_PROP_FRAME_HEIGHT, SIZE[0])
+        self.consumer.set(cv2.CAP_PROP_FRAME_WIDTH, SIZE[0])
+        self.consumer.set(cv2.CAP_PROP_FRAME_HEIGHT, SIZE[1])
 
-    def _receive(self):
-        return rstate.randint(0,256,size=SIZE,dtype='uint8')
-
-    async def receive(self):
-        await asyncio.sleep(50 / 1e3)
-        return await self._loop.run_in_executor(None, self._receive)
-
-class MyInput(CsvLogging, Producer):
+class MyFrameProducer(CsvLogging, Producer):
     def __init__(self, consumer, loop=None):
         self.consumer = consumer
-        self.cam_id = f"cam_{os.getenv('CAM_ID', str(uuid.uuid4()))}"
+        self.cam_id = get_cam_id()
         self.producer_topic = 'input'
         # self.producer_conf = {'max_request_size':10000000}
         self.frame_idx = 1
@@ -45,18 +32,34 @@ class MyInput(CsvLogging, Producer):
 
     async def send(self, data):
         key = str(self.frame_idx).encode()
-        now = datetime.utcnow()
-        strf = datetime.strftime(now, '%Y-%m-%d %H:%M:%S.%f')
         headers = [
             ('cam', self.cam_id.encode()),
             ('frame', str(self.frame_idx).encode()),
-            ('timestamp', strf.encode())]
+            ('timestamp', get_timestamp_str().encode())]
         await super().send(data, key=key, headers=headers)
         self.frame_idx += 1
+# ======================================================================
+class MyFrameProducerSc2_4(MyFrameProducer):
+    def __init__(self, consumer, loop=None):
+        self.producer_servers = os.getenv('LOCAL_KAFKA')
+        super().__init__(consumer, loop)
+
+class MyFrameProducerSc3(MyFrameProducer):
+    def __init__(self, consumer, loop=None):
+        self.producer_servers = os.getenv('CLOUD_KAFKA')
+        super().__init__(consumer, loop)
+
+scenarios = {
+    2: (MyStorage, MyFrameProducerSc2_4),
+    3: (MyStorage, MyFrameProducerSc3),
+    4: (MyStorage, MyFrameProducerSc2_4),
+}
 
 async def main():
-    consumer = MyConsumer()
-    producer = MyInput(consumer)
+    scenario = int(os.getenv('SCENARIO', 4))
+    _Consumer, _Producer = scenarios[scenario]
+    consumer = _Consumer()
+    producer = _Producer(consumer)
     tasks = [consumer.run(), producer.run()]
     try:
         await asyncio.gather(*tasks)
