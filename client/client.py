cb31ae52c84cd33efc07c65d9bafa886ca88c12f
ariqbasyar <ariqbasyar2@gmail.com>
2022-06-20 20:08:12 +0700

    done client with flask socketio, asyncio, and threading

--- client/client.py
+++ client/client.py
@@ -1,46 +1,61 @@
-from time import time
-import uuid
-import base64
-import cv2
+import asyncio
 import threading
-
-import numpy as np
+import uuid
 
 from fogverse import Consumer
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
+
+def page_not_found(e):
+  return render_template('404.html'), 404
 
 app = Flask(__name__)
+app.register_error_handler(404, page_not_found)
 socketio = SocketIO(app)
 
-class MyConsumer(Consumer, threading.Thread):
-    def __init__(self):
-        self.consumer_topic = ['input']
+class MyConsumer(Consumer):
+    def __init__(self, socket: SocketIO, loop=None):
+        self.socket = socket
+        self.sockets = {}
         self.auto_encode = False
-        self.consumer_conf = {'group.id': str(uuid.uuid4())}
-        Consumer.__init__(self)
-        threading.Thread.__init__(self)
+        self.topic_pattern = '^final_cam_[0-9a-zA-Z-]+$'
+        self.consumer_conf = {'group_id': str(uuid.uuid4())}
+        super().__init__(loop=loop)
+        self.counter = 1
 
-    def process(self, data):
-        assert isinstance(data, np.ndarray), 'data should be in np.ndarray type'
-        encoding = 'jpg'
-        success, img_webp = cv2.imencode(f'.{encoding}', data)
-        assert success, f'failed to encode image as {encoding}'
-        b64_img = base64.b64encode(img_webp).decode()
-        return f'data:image/{encoding};base64,{b64_img}'
+    async def send(self, data):
+        namespace = f'/{self.message.topic}'
+        headers = self.message.headers
+        headers = {key: value.decode() for key, value in headers}
+        data = {
+            'src': data,
+            'headers': headers,
+        }
+        self.socket.emit('frame', data, namespace=namespace)
 
-    def send(self, data):
-        socketio.emit('frame', {'data': data}, namespace='/video')
-
-@app.route('/')
-def index():
+@app.route('/<cam_id>/')
+def index(cam_id=None):
+    if not cam_id:
+        return render_template('404.html')
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/video')
-def on_connected(*args):
-    emit('connected', namespace='/video')
+async def main(loop):
+    consumer = MyConsumer(socketio, loop=loop)
+    tasks = [consumer.run()]
+    try:
+        await asyncio.gather(*tasks)
+    finally:
+        for t in tasks:
+            t.close()
+
+def run_consumer(loop):
+    try:
+        loop.run_until_complete(main(loop))
+    finally:
+        loop.close()
 
 if __name__ == '__main__':
-    consumer = MyConsumer()
-    consumer.start()
+    loop = asyncio.new_event_loop()
+    thread = threading.Thread(target=run_consumer, args=(loop,))
+    thread.start()
     socketio.run(app, debug=True, host='0.0.0.0', use_reloader=False)
