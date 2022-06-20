847984456c933631923a138154633af5cfaa2b95
ariqbasyar <ariqbasyar2@gmail.com>
2022-06-20 20:32:51 +0700

    jetson for scenario 2 and 4 is ready

--- fog/jetson_nano/inference.py
+++ fog/jetson_nano/inference.py
@@ -1,13 +1,14 @@
 import os
 import asyncio
-import cv2
 import torch
 
 from fogverse import Consumer, Producer, ConsumerStorage
 from fogverse.logging.logging import CsvLogging
-from fogverse.util import numpy_to_bytes
+from fogverse.util import get_header, numpy_to_base64_url
 
-class MyConsumer(Consumer, ConsumerStorage):
+ENCODING = os.getenv('ENCODING', 'jpg')
+
+class MyStorage(Consumer, ConsumerStorage):
     def __init__(self, keep_messages=False):
         self.consumer_topic = ['input']
         Consumer.__init__(self)
@@ -17,7 +18,6 @@ class MyJetson(CsvLogging, Producer):
     def __init__(self, consumer):
         MODEL = os.getenv('MODEL', 'yolov5n')
         self.model = torch.hub.load('ultralytics/yolov5', MODEL)
-        self.producer_topic = 'result'
         self.consumer = consumer
         CsvLogging.__init__(self)
         Producer.__init__(self)
@@ -35,17 +35,38 @@ class MyJetson(CsvLogging, Producer):
                                                data)
 
     def encode(self, img):
-        _, encoded = cv2.imencode('.jpg', img)
-        return numpy_to_bytes(encoded)
+        return numpy_to_base64_url(img, ENCODING).encode()
 
     async def send(self, data):
         headers = list(self.message.headers)
         headers.append(('type',b'final'))
         await super().send(data, headers=headers)
+# ======================================================================
+class MyJetsonSc2(MyJetson):
+    def __init__(self, consumer):
+        super().__init__(consumer)
+
+    async def send(self, data):
+        headers = self.message.headers
+        cam_id = get_header(headers, 'cam')
+        self.producer_topic = f'final_{cam_id}'
+        await super().send(data)
+# ======================================================================
+class MyJetsonSc4(MyJetson):
+    def __init__(self, consumer):
+        self.producer_topic = 'result'
+        super().__init__(consumer)
+
+scenarios = {
+    2: (MyStorage, MyJetsonSc2),
+    4: (MyStorage, MyJetsonSc4),
+}
 
 async def main():
-    consumer = MyConsumer()
-    producer = MyJetson(consumer)
+    scenario = int(os.getenv('SCENARIO', 4))
+    _Consumer, _Producer = scenarios[scenario]
+    consumer = _Consumer()
+    producer = _Producer(consumer)
     tasks = [consumer.run(), producer.run()]
     try:
         await asyncio.gather(*tasks)
