be818e94d84149bc4a3e73e97f69ad4a1dbffb73
ariqbasyar <ariqbasyar2@gmail.com>
2022-06-27 17:35:36 +0700

    added v9 data and modified v8 filename data for future compatibility

--- analysis/copy-logs.sh
+++ analysis/copy-logs.sh
@@ -1,13 +1,38 @@
 ROOT_PROJECT=/home/ariqbasyar/Documents/kuliah/term_8/tugas_akhir/fogverse
-TO=$ROOT_PROJECT/analysis/$1/
+TO=$ROOT_PROJECT/analysis/$1/scenario-$2/
 echo WARNING! selected folder $1
 echo WARNING! copying logs to $TO
 set -xe
 sleep 10
-scp debian-camera:~/fogverse/logs/log_MyFrameProducer.csv $TO
-scp raspi:~/fogverse/logs/log_MyPreprocess.csv $TO
-scp jetson:~/fogverse/logs/log_MyJetson.csv $TO
-scp jetson:~/fogverse/logs/log_MyJetsonScenario1.csv $TO
-scp gcloud-server-1:~/fogverse/logs/log_MyCloud.csv $TO
-scp debian-master:~/fogverse/logs/log_MyForwarder.csv $TO
-scp debian-master:~/fogverse/logs/log_MyMerger.csv $TO
+
+case $2 in
+    1)
+        # skenario 1
+        scp jetson:~/fogverse/logs/log_MyJetsonSc1.csv $TO
+        ;;
+    2)
+        # skenario 2
+        scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc2_4.csv $TO
+        scp jetson:~/fogverse/logs/log_MyJetsonSc2.csv $TO
+        cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
+        ;;
+    3)
+        # skenario 3
+        scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc3.csv $TO
+        scp gcloud-server-1:~/fogverse/logs/log_MyCloudSc3.csv $TO
+        cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
+        ;;
+    4)
+        # skenario 4
+        scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc2_4.csv $TO
+        scp raspi:~/fogverse/logs/log_MyPreprocess.csv $TO
+        scp jetson:~/fogverse/logs/log_MyJetsonSc4.csv $TO
+        scp gcloud-server-1:~/fogverse/logs/log_MyCloudSc4.csv $TO
+        scp debian-master:~/fogverse/logs/log_MyForwarder.csv $TO
+        scp debian-master:~/fogverse/logs/log_MyMerger.csv $TO
+        cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
+        ;;
+    *)
+        echo "unknown scenario"
+        ;;
+esac
