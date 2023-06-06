69569c750fe82004b372cc2d5e6e7a6e200bc0f6
ariqbasyar <ariqbasyar2@gmail.com>
2023-06-06 16:13:23 +0700

    read images from custom dir, not camera

--- input/producer.py
+++ input/producer.py
@@ -4,31 +4,41 @@ import cv2
 
 import numpy as np
 
-from fogverse import ConsumerStorage, Producer, OpenCVConsumer
+from pathlib import Path
+from fogverse import Producer
 from fogverse.logging import CsvLogging
-from fogverse.util import get_cam_id, get_timestamp_str
+from fogverse.util import (get_cam_id, get_timestamp_str)
 
-SIZE = (640,480,3)
-
-class MyStorage(OpenCVConsumer, ConsumerStorage):
-    def __init__(self):
-        OpenCVConsumer.__init__(self)
-        ConsumerStorage.__init__(self)
-        self.consumer.set(cv2.CAP_PROP_FRAME_WIDTH, SIZE[0])
-        self.consumer.set(cv2.CAP_PROP_FRAME_HEIGHT, SIZE[1])
+SIZE = (640,480)
+DIR = Path('val2017')
 
 class MyFrameProducer(CsvLogging, Producer):
-    def __init__(self, consumer, loop=None):
-        self.consumer = consumer
+    def __init__(self, loop=None):
         self.cam_id = get_cam_id()
         self.producer_topic = 'input'
-        # self.producer_conf = {'max_request_size':10000000}
         self.frame_idx = 1
         CsvLogging.__init__(self)
         Producer.__init__(self,loop=loop)
 
+    def _after_start(self):
+        with open('images.txt') as f:
+            self.images_name = f.read().split(',')
+
+    def _receive(self):
+        img_name = self.images_name.pop(0)
+        with open(str(DIR / img_name), 'rb') as f:
+            data = f.read()
+        return data
+
     async def receive(self):
-        return await self.consumer.get()
+        return await self._loop.run_in_executor(None, self._receive)
+
+    def decode(self, data):
+        nparr = np.frombuffer(data, np.uint8)
+        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, SIZE)
+        return frame
 
     async def send(self, data):
         key = str(self.frame_idx).encode()
@@ -38,29 +48,31 @@ class MyFrameProducer(CsvLogging, Producer):
             ('timestamp', get_timestamp_str().encode())]
         await super().send(data, key=key, headers=headers)
         self.frame_idx += 1
+
 # ======================================================================
 class MyFrameProducerSc2_4(MyFrameProducer):
-    def __init__(self, consumer, loop=None):
+    def __init__(self, loop=None):
         self.producer_servers = os.getenv('LOCAL_KAFKA')
-        super().__init__(consumer, loop)
+        super().__init__(loop)
 
 class MyFrameProducerSc3(MyFrameProducer):
-    def __init__(self, consumer, loop=None):
+    def __init__(self, loop=None):
         self.producer_servers = os.getenv('CLOUD_KAFKA')
-        super().__init__(consumer, loop)
+        super().__init__(loop)
+
 
 scenarios = {
-    2: (MyStorage, MyFrameProducerSc2_4),
-    3: (MyStorage, MyFrameProducerSc3),
-    4: (MyStorage, MyFrameProducerSc2_4),
+    2: (MyFrameProducerSc2_4),
+    3: (MyFrameProducerSc3),
+    4: (MyFrameProducerSc2_4),
 }
 
 async def main():
     scenario = int(os.getenv('SCENARIO', 4))
-    _Consumer, _Producer = scenarios[scenario]
-    consumer = _Consumer()
-    producer = _Producer(consumer)
-    tasks = [consumer.run(), producer.run()]
+
+    _Producer = scenarios[scenario]
+    producer = _Producer()
+    tasks = [producer.run()]
     try:
         await asyncio.gather(*tasks)
     except:
