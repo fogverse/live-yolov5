ab5089c11a0e5b934e509270b7218c477e3ff097
ariqbasyar <ariqbasyar2@gmail.com>
2023-07-06 11:52:41 +0700

    copy log script set root project relative to this file

--- analysis/copy-logs.sh
+++ analysis/copy-logs.sh
@@ -1,4 +1,6 @@
-ROOT_PROJECT=/home/ariqbasyar/Documents/kuliah/term_8/tugas_akhir/fogverse
+# ROOT_PROJECT=$(dirname $(realpath $0))/..
+ROOT_PROJECT=$(dirname $0)/..
+ROOT_PROJECT=$(cd $ROOT_PROJECT && pwd)
 TO=$ROOT_PROJECT/analysis/$1/scenario-$2/
 echo WARNING! selected folder $1
 echo WARNING! copying logs to $TO
@@ -7,27 +9,27 @@ sleep 10
 
 case $2 in
     1)
-        # skenario 1
+        # scenario 1
         scp jetson:~/fogverse/logs/log_MyJetsonSc1.csv $TO
         ;;
     2)
-        # skenario 2
+        # scenario 2
         scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc2_4.csv $TO
         scp jetson:~/fogverse/logs/log_MyJetsonSc2.csv $TO
         cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
         ;;
     3)
-        # skenario 3
+        # scenario 3
         scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc3.csv $TO
         scp gcloud-server-1:~/fogverse/logs/log_MyCloudSc3.csv $TO
         cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
         ;;
     4)
-        # skenario 4
+        # scenario 4
         scp debian-camera:~/fogverse/logs/log_MyFrameProducerSc2_4.csv $TO
-        scp raspi:~/fogverse/logs/log_MyPreprocess.csv $TO
-        scp jetson:~/fogverse/logs/log_MyJetsonSc4.csv $TO
-        scp gcloud-server-1:~/fogverse/logs/log_MyCloudSc4.csv $TO
+        scp raspi:~/studi-mandiri/logs/log_MyPreprocess.csv $TO
+        scp jetson:~/Documents/kuliah/term_9/studi-mandiri/logs/log_MyJetsonSc4.csv $TO
+        gcloud compute scp cloud-jakarta:~/fogverse/logs/log_MyCloudSc4.csv $TO
         scp debian-master:~/fogverse/logs/log_MyForwarder.csv $TO
         scp debian-master:~/fogverse/logs/log_MyMerger.csv $TO
         cp $ROOT_PROJECT/client/logs/log_MyClient.csv $TO
