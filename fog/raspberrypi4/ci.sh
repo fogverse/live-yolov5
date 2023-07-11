be3fc41fd906ce028d3871d56c7e686dc24630a1
ariqbasyar <ariqbasyar2@gmail.com>
2023-07-11 17:21:06 +0700

    ci for rasp self study

--- fog/raspberrypi4/ci.sh
+++ fog/raspberrypi4/ci.sh
@@ -3,6 +3,6 @@ echo $DREG
 docker buildx build \
     --platform linux/arm64 \
     --build-arg DREG=$DREG \
-    -t ${DREG}ariqbasyar/fogverse:preprocess-raspberrypi \
+    -t ${DREG}ariqbasyar/fogverse:preprocess-raspberrypi-sm \
     -f fog/raspberrypi4/Dockerfile \
     --push .
